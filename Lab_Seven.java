3fcf51a74db41cca8770891af0f8dab3468cd6f9
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2020-04-27 22:13:33 -0700

    Add files via upload

--- Lab_Seven.java
+++ Lab_Seven.java
@@ -1,5 +1,7 @@
 package cecs328;
 
+package cecs328;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -82,17 +84,93 @@ public class Lab_Seven {
 		}	
 	}
 	
+	
 		
-	public static void explore() {
-		//Initialize all nodes to have all color gray
+	public static void explore(Node v) {
+		//Initialize vertex (parameter) to have gray
+		v.color = 0;	
+		
+		//Change vertex color to blue
+		v.color = 1;
+		
+		//Call is_bipartite on it
+		if(is_bipartite(v)) {
+			
+		}else {
+			
+		}
+		
 	}
 	
-	public static boolean is_bipartite() {
+	public static boolean is_bipartite(Node s) {
+		//Creates the queue used for bfs
+		Queue<Node> q = new LinkedList<Node>();
 		
+		//Set Distance to 0
+		s.dst = 0;
+		
+		//Set s to be its own parent
+		s.parent = s;
+		
+		//Push node into the queue
+		q.add(s);
+		
+		while(!q.isEmpty()) {
+			//Stores first node from the queue
+			System.out.print("Queue: ");
+			print_queue(q);
+			
+			//Part a - Keep popping each vertex from queue
+			Node u = q.poll();
+			
+			//Gives nullpointexception if not included
+			if(u.adj != null) {
+				//V are neighbors of u
+				for(Node v : u.adj) {
+					if(!v.hasParent()) {
+						//Part C - Assigning a opposite color to V (u is blue)
+						if(v.color == 0) {
+							v.color = 1;		//neighbor nodes are red
+						}
+						
+						if(v.color == u.color) {	//Neighboring nodes can not be the same color to be bipartite
+							System.out.println("NOT Bipartite");
+							return false;
+						}
+						q.add(v);
+					}		
+				}	
+			}
+		}	
+
+		return true;
 		
 	}
+	
 	public static void main(String[] args) {
 		//Part A
+//		Node a_n = new Node("a");
+//		Node b_n = new Node("b");
+//		Node c_n = new Node("c");
+//		Node d_n = new Node("d");
+//		Node e_n = new Node("e");
+//		Node f_n = new Node("f");
+//		Node g_n = new Node("g");
+//		Node h_n = new Node("h");
+//	
+//		
+//		a_n.adj = new Node[] {c_n, d_n};	
+//		b_n.adj = new Node[] {c_n, e_n};
+//		c_n.adj = new Node[] {a_n, b_n, d_n};
+//		d_n.adj = new Node[] {a_n, c_n, e_n, f_n};
+//		e_n.adj = new Node[] {b_n, d_n, f_n};
+//		f_n.adj = new Node[] {d_n, e_n, h_n};
+//		h_n.adj = new Node[] {f_n};
+		
+//		BFS(a_n);
+		
+		
+		//Part B - Bipartite
 		Node a_n = new Node("a");
 		Node b_n = new Node("b");
 		Node c_n = new Node("c");
@@ -101,28 +179,43 @@ public class Lab_Seven {
 		Node f_n = new Node("f");
 		Node g_n = new Node("g");
 		Node h_n = new Node("h");
-	
+		Node i_n = new Node("i");
 		
-		a_n.adj = new Node[] {c_n, d_n};	
-		b_n.adj = new Node[] {c_n, e_n};
-		c_n.adj = new Node[] {a_n, b_n, d_n};
-		d_n.adj = new Node[] {a_n, c_n, e_n, f_n};
-		e_n.adj = new Node[] {b_n, d_n, f_n};
-		f_n.adj = new Node[] {d_n, e_n, h_n};
-		h_n.adj = new Node[] {f_n};
+		a_n.adj = new Node[] {d_n};	
+		b_n.adj = new Node[] {d_n, f_n};
+		c_n.adj = new Node[] {d_n, e_n};
+		d_n.adj = new Node[] {a_n, c_n, b_n};
+		e_n.adj = new Node[] {c_n};
+		f_n.adj = new Node[] {b_n};
+		h_n.adj = new Node[] {g_n, i_n};
+		i_n.adj = new Node[] {g_n, h_n};
+		g_n.adj = new Node[] {h_n, i_n};
 		
-		BFS(a_n);
+		explore(a_n);
+		explore(b_n);
+		explore(c_n);
+		explore(d_n);
+		explore(e_n);
+		explore(f_n);
+		explore(g_n);
+		explore(h_n);
+		explore(i_n);
 		
-		
-		//Part B - Bipartite
-		
-		
-		
-		
-		
+		//Printing Color
+		System.out.println("Colors: 0 = Gray, 1 = Blue, 2 = Red");
+		System.out.println("Node a: " + a_n.color);
+		System.out.println("Node b: " + b_n.color);
+		System.out.println("Node c: " + c_n.color);
+		System.out.println("Node d: " + d_n.color);
+		System.out.println("Node e: " + e_n.color);
+		System.out.println("Node f: " + f_n.color);
+		System.out.println("Node g: " + g_n.color);
+		System.out.println("Node h: " + h_n.color);
+		System.out.println("Node i: " + i_n.color);
 		
 		
 	}
 	
 	
 }
+
